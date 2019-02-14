f3edff5c713c36d7811621884485c699b87fc52d
QuantumStar <JWSwafford01@gmail.com>
2019-02-14 03:14:24 -0800

    Update goodVsEvil.py

--- goodVsEvil.py
+++ goodVsEvil.py
@@ -1,5 +1,10 @@
 # Original concept and code by Wesley Swafford, 2017
 #
+# This program generates a random number between 1 and 999,999. If the number is equal to 666, one point is added to the 'evilCount'
+# counter. If is is equal to 777, one point is added to the 'goodCount' counter. Whenever either comes up, it is printed to the console,
+# and the score is shown. The score is a positive or negative integer, and shows how far 'ahead' one may be. Whichever counter reaches 99
+# first is the 'winner', and a message is displayed with the outcome.
+#
 # The only difference between this version and the 'fast' version is that this has a
 # wider range of random numbers (1, 999999). This is so modern computers don't run the program
 # so quickly that the process is too fast to see. The 'fast' version is better for the Raspberry Pi
